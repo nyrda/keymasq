264a7bd82521b535318bd93cf1b8f32800588b0d
nyrda <nyrda@keyforge.tools>
2026-04-13 08:11:10 +0200

    Bump version to v0.3.2

--- PKGBUILD
+++ PKGBUILD
@@ -3,7 +3,7 @@
 
 # Maintainer: nyrda <nyrda@keyforge.tools>
 pkgname=keyforge
-pkgver=0.3.1
+pkgver=0.3.2
 pkgrel=1
 pkgdesc="A key remapping tool for Linux using evdev and uinput"
 arch=(
--- keyforge/gui/application.py
+++ keyforge/gui/application.py
@@ -23,7 +23,7 @@ from keyforge.gui.icons import register_icon_search_path, theme_supports_core_ic
 from keyforge.gui.session_reload import notify_session_reload_async  # noqa: E402
 from keyforge.gui.window import MainWindow  # noqa: E402
 
-APP_VERSION = "0.3.1"
+APP_VERSION = "0.3.2"
 
 
 class Application(Adw.Application):

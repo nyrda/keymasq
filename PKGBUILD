2e150fe4461b4c41edc7687701c2143ce54e39ec
nyrda <nyrda@keyforge.tools>
2026-04-16 02:25:48 +0200

    Bump version to v0.4.0

--- PKGBUILD
+++ PKGBUILD
@@ -3,7 +3,7 @@
 
 # Maintainer: nyrda <nyrda@keymasq.tools>
 pkgname=keymasq
-pkgver=0.3.2
+pkgver=0.4.0
 pkgrel=1
 pkgdesc="A key remapping tool for Linux using evdev and uinput"
 arch=(
--- keymasq/gui/application.py
+++ keymasq/gui/application.py
@@ -23,7 +23,7 @@ from keymasq.gui.icons import register_icon_search_path, theme_supports_core_ico
 from keymasq.gui.session_reload import notify_session_reload_async  # noqa: E402
 from keymasq.gui.window import MainWindow  # noqa: E402
 
-APP_VERSION = "0.3.2"
+APP_VERSION = "0.4.0"
 
 
 class Application(Adw.Application):

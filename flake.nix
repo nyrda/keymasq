4b6f17c88dddc43c0ce361c575924e5c548aba49
nyrda <nyrda@keymasq.tools>
2026-04-23 03:48:42 +0200

    Bump version to v0.8.1

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
         in
         runtimePythonPackages.buildPythonPackage {
           pname = "keymasq";
-          version = "0.8.0";
+          version = "0.8.1";
           pyproject = true;
 
           src = mkCleanSrc pkgs;

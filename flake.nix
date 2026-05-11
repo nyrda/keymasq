72d19d5561e1f38d44b44114db32ac888d282833
nyrda <nyrda@keymasq.tools>
2026-05-11 16:53:12 +0200

    Bump version to 0.17.1
    - Add 0.17.1 release notes
    - Update package metadata and source URLs

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
         in
         runtimePythonPackages.buildPythonPackage {
           pname = "keymasq";
-          version = "0.17.0";
+          version = "0.17.1";
           pyproject = true;
 
           src = mkCleanSrc pkgs;
